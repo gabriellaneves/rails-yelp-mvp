dba94bb289b0dce7fde8fa65b7ed7d56eb81579f
gabriellaneves <gabriella.nunes@gmail.com>
2020-09-17 15:01:02 -0300

    yeld

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+    belongs_to :restaurant
+    
+    validates :content, :rating, presence: true
+    validates :rating, numericality: { only_integer: true }
+    validates :rating, inclusion: { in: (0..5).to_a,
+    message: "%{value} is not a valid number" }
+       
+end
